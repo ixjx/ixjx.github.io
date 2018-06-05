203a8f1a976521d81c6141aac13f90b3b2afd5dc
ixjx <vissky1@163.com>
2018-06-05 18:47:02 +0800

    sinaapp enforce https

--- resume/js/app.js
+++ resume/js/app.js
@@ -1,6 +1,6 @@
 var deerResume = angular.module('deerResume', ['ngRoute','wiz.markdown','ngNotify','angularLocalStorage']);
 
-var baseurl = 'http://cvbox.sinaapp.com/'; // 使用SAE托管简历数据
+var baseurl = 'https://cvbox.sinaapp.com/'; // 使用SAE托管简历数据
 //var baseurl = 'data.php'; // 使用本地文件托管简历数据，本地模式下，不支持在线编辑
 
 
