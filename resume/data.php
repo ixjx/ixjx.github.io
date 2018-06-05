7f620357fa50faebfa8f43848e9cdf4e6373e15b
ixjx <vissky1@163.com>
2018-06-05 16:11:19 +0800

    local edit

--- resume/data.php
+++ resume/data.php
@@ -1,8 +1,8 @@
 <?php
 
-$viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
-$title = '标题';
-$subtitle = '子标题';
+$viewpass = '1225'; // 密码必须大于3位。留空则任何人可以访问
+$title = '个人简历';
+$subtitle = '个人简历';
 $content = 'MarkDown';
 
 $data['local'] = 1;
